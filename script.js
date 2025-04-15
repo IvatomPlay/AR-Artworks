8b518725a2e295016781283ba64a10624754286c
Ivan Tomtosov <ivatom@MacBook-Air-Ivan.local>
2025-04-16 08:32:42 +0900

    now pressing button will not return you to a top of the page

--- script.js
+++ script.js
@@ -38,12 +38,14 @@ document.addEventListener("DOMContentLoaded", () => {
     `;
   }
 
-  prevBtn.addEventListener("click", () => {
+  prevBtn.addEventListener("click", (e) => {
+    e.preventDefault();
     currentIndex = (currentIndex - 1 + images.length) % images.length;
     showImage(currentIndex);
   });
 
-  nextBtn.addEventListener("click", () => {
+  nextBtn.addEventListener("click", (e) => {
+    e.preventDefault();
     currentIndex = (currentIndex + 1) % images.length;
     showImage(currentIndex);
   });
